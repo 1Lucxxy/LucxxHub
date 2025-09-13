7a6b7186d59a45efa4a2e4934425083ee30d4b8d
1Lucxxy <dafaraza61@gmail.com>
2025-09-14 04:17:03 +0700

    LucxxHub.lua

--- LucxxHub.lua
+++ LucxxHub.lua
@@ -125,160 +125,34 @@ task.spawn(function()
 end)
 
 ------------------------------------------------
---// VISUAL TAB (ESP)
+--// VISUAL TAB (Highlight ESP)
 ------------------------------------------------
 local VisualTab = Window:CreateTab("Visual", 4483362458)
 
 -- ESP Variables
-local BoxESPEnabled = false
+local HighlightESPEnabled = false
 local HealthESPEnabled = false
 local NameESPEnabled = false
-local BoxColor = Color3.fromRGB(0,255,0)
-local Drawings = {}
+local HighlightColor = Color3.fromRGB(0,255,0)
+local ESPData = {}
 
--- Remove ESP
-local function removeESP(plr)
-    if Drawings[plr] then
-        for _, obj in pairs(Drawings[plr]) do
-            if obj.Remove then obj:Remove() end
-        end
-        Drawings[plr] = nil
+-- Buat Highlight untuk player
+local function createHighlight(plr)
+    if ESPData[plr] then return end
+    local hl = Instance.new("Highlight")
+    hl.FillColor = HighlightColor
+    hl.FillTransparency = 0.5
+    hl.OutlineColor = Color3.fromRGB(255,255,255)
+    hl.OutlineTransparency = 0
+    hl.Enabled = false
+    if plr.Character then
+        hl.Parent = plr.Character
     end
+    ESPData[plr] = {Highlight = hl, Health = nil, Name = nil}
 end
 
--- Add ESP
-local function addESP(plr)
-    if plr == game.Players.LocalPlayer then return end
-
-    local box = Drawing.new("Square")
-    box.Thickness = 2
-    box.Color = BoxColor
-    box.Filled = false
-    box.Visible = false
-
-    local healthBar = Drawing.new("Square")
-    healthBar.Thickness = 1
-    healthBar.Filled = true
-    healthBar.Visible = false
-
-    local nameText = Drawing.new("Text")
-    nameText.Size = 15
-    nameText.Center = true
-    nameText.Outline = true
-    nameText.Color = Color3.fromRGB(255, 255, 255)
-    nameText.Visible = false
-    nameText.Text = plr.Name
-
-    Drawings[plr] = {box, healthBar, nameText}
-
-    game:GetService("RunService").RenderStepped:Connect(function()
-        if plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") then
-            local hrp = plr.Character.HumanoidRootPart
-            local humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
-            local head = plr.Character:FindFirstChild("Head")
-
-            if humanoid and head then
-                local pos, vis = workspace.CurrentCamera:WorldToViewportPoint(hrp.Position)
-                if vis then
-                    local scale = 1 / (pos.Z * 0.003) * 100
-                    local width, height = 30 * scale, 50 * scale
-
-                    -- Box ESP
-                    if BoxESPEnabled then
-                        box.Size = Vector2.new(width, height)
-                        box.Position = Vector2.new(pos.X - width / 2, pos.Y - height / 2)
-                        box.Color = BoxColor
-                        box.Visible = true
-                    else
-                        box.Visible = false
-                    end
-
-                    -- Healthbar (kiri box)
-                    if HealthESPEnabled then
-                        local hpPercent = humanoid.Health / humanoid.MaxHealth
-                        local hbHeight = height * hpPercent
-                        local hbWidth = 4
-                        local hbX = pos.X - width / 2 - hbWidth - 2
-                        local hbY = pos.Y + height / 2 - hbHeight
-
-                        healthBar.Size = Vector2.new(hbWidth, hbHeight)
-                        healthBar.Position = Vector2.new(hbX, hbY)
-
-                        if hpPercent > 0.5 then
-                            healthBar.Color = Color3.fromRGB(0, 255, 0)
-                        elseif hpPercent > 0.2 then
-                            healthBar.Color = Color3.fromRGB(255, 255, 0)
-                        else
-                            healthBar.Color = Color3.fromRGB(255, 0, 0)
-                        end
-
-                        healthBar.Visible = true
-                    else
-                        healthBar.Visible = false
-                    end
-
-                    -- Name di atas kepala
-                    if NameESPEnabled then
-                        local headPos, vis2 = workspace.CurrentCamera:WorldToViewportPoint(head.Position + Vector3.new(0, 1, 0))
-                        if vis2 then
-                            nameText.Position = Vector2.new(headPos.X, headPos.Y - 15)
-                            nameText.Visible = true
-                        else
-                            nameText.Visible = false
-                        end
-                    else
-                        nameText.Visible = false
-                    end
-                else
-                    box.Visible = false
-                    healthBar.Visible = false
-                    nameText.Visible = false
-                end
-            end
-        else
-            box.Visible = false
-            healthBar.Visible = false
-            nameText.Visible = false
-        end
-    end)
-end
-
--- Auto Apply ESP
-for _, plr in pairs(game.Players:GetPlayers()) do
-    addESP(plr)
-end
-game.Players.PlayerAdded:Connect(addESP)
-game.Players.PlayerRemoving:Connect(removeESP)
-
--- UI Toggles
-VisualTab:CreateToggle({
-    Name = "ESP Box",
-    CurrentValue = false,
-    Callback = function(Value)
-        BoxESPEnabled = Value
-    end,
-})
-
-VisualTab:CreateColorPicker({
-    Name = "Box Color",
-    Color = Color3.fromRGB(0,255,0),
-    Callback = function(Value)
-        BoxColor = Value
-    end
-})
-
-VisualTab:CreateToggle({
-    Name = "ESP Healthbar",
-    CurrentValue = false,
-    Callback = function(Value)
-        HealthESPEnabled = Value
-    end,
-})
-
-VisualTab:CreateToggle({
-    Name = "ESP Name",
-    CurrentValue = false,
-    Callback = function(Value)
-        NameESPEnabled = Value
-    end,
-})
+-- Update ESP tiap frame
+game:GetService("RunService").RenderStepped:Connect(function()
+    for _, plr in pairs(game.Players:GetPlayers()) do
+        if plr ~= game.Players.LocalPlayer then
+            if not ESPData[plr] then createHi
