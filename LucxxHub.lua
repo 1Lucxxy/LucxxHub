fa2f8887a7b268807394020d15d59a9f6e499b9b
1Lucxxy <dafaraza61@gmail.com>
2025-09-14 04:13:28 +0700

    LucxxHub.lua

--- LucxxHub.lua
+++ LucxxHub.lua
@@ -1,36 +1,40 @@
 --// Load Rayfield
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
---// Buat Window
+--// Window
 local Window = Rayfield:CreateWindow({
-   Name = "Lucxx Hub",
-   LoadingTitle = "Lucxx UI",
-   LoadingSubtitle = "by Lucxxy",
+   Name = "Universal Hub",
+   LoadingTitle = "Rayfield UI",
+   LoadingSubtitle = "by kamu",
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil,
-      FileName = "LucxxConfig"
+      FileName = "UniversalConfig"
    },
    KeySystem = false
 })
 
-------------------------------------------------------
---// Tab Player
+------------------------------------------------
+--// PLAYER TAB
+------------------------------------------------
 local PlayerTab = Window:CreateTab("Player", 4483362458)
-local PlayerSection = PlayerTab:CreateSection("Movement")
+local Section = PlayerTab:CreateSection("Movement")
 
--- Variabel Movement
+-- Variables
 local SelectedSpeed = 16
 local SelectedJump = 50
 local SelectedGravity = 196.2
+local SelectedZoom = 128
 local Running = true
 
--- WalkSpeed
+-- WalkSpeed Slider
 PlayerTab:CreateSlider({
    Name = "WalkSpeed",
    Range = {16, 300},
    Increment = 1,
+   Suffix = "Speed",
    CurrentValue = 16,
+   Flag = "WalkSpeedSlider",
    Callback = function(Value)
       SelectedSpeed = Value
       local plr = game.Players.LocalPlayer
@@ -40,12 +44,14 @@ PlayerTab:CreateSlider({
    end,
 })
 
--- JumpPower
+-- JumpPower Slider
 PlayerTab:CreateSlider({
    Name = "JumpPower",
    Range = {50, 200},
    Increment = 1,
+   Suffix = "Jump",
    CurrentValue = 50,
+   Flag = "JumpPowerSlider",
    Callback = function(Value)
       SelectedJump = Value
       local plr = game.Players.LocalPlayer
@@ -58,19 +64,35 @@ PlayerTab:CreateSlider({
    end,
 })
 
--- Gravity
+-- Gravity Slider
 PlayerTab:CreateSlider({
    Name = "Gravity",
    Range = {0, 500},
    Increment = 1,
+   Suffix = "G",
    CurrentValue = 196,
+   Flag = "GravitySlider",
    Callback = function(Value)
       SelectedGravity = Value
       workspace.Gravity = SelectedGravity
    end,
 })
 
--- Button Fly
+-- Max Zoom
+PlayerTab:CreateSlider({
+   Name = "Max Zoom",
+   Range = {0, 1000},
+   Increment = 10,
+   Suffix = "Zoom",
+   CurrentValue = 128,
+   Flag = "ZoomSlider",
+   Callback = function(Value)
+      SelectedZoom = Value
+      game.Players.LocalPlayer.CameraMaxZoomDistance = SelectedZoom
+   end,
+})
+
+-- Fly Button
 PlayerTab:CreateButton({
    Name = "Fly",
    Callback = function()
@@ -78,7 +100,7 @@ PlayerTab:CreateButton({
    end,
 })
 
--- Anti-reset loop
+-- Anti reset loop
 task.spawn(function()
    while Running do
       task.wait(0.2)
@@ -96,20 +118,25 @@ task.spawn(function()
       if workspace.Gravity ~= SelectedGravity then
          workspace.Gravity = SelectedGravity
       end
+      if plr.CameraMaxZoomDistance ~= SelectedZoom then
+         plr.CameraMaxZoomDistance = SelectedZoom
+      end
    end
 end)
 
-------------------------------------------------------
---// Tab Visual
+------------------------------------------------
+--// VISUAL TAB (ESP)
+------------------------------------------------
 local VisualTab = Window:CreateTab("Visual", 4483362458)
-local VisualSection = VisualTab:CreateSection("ESP & Camera")
 
--- Variabel ESP
-local ESPEnabled = false
-local BoxColor = Color3.fromRGB(0, 255, 0)
+-- ESP Variables
+local BoxESPEnabled = false
+local HealthESPEnabled = false
+local NameESPEnabled = false
+local BoxColor = Color3.fromRGB(0,255,0)
 local Drawings = {}
 
--- Hapus ESP
+-- Remove ESP
 local function removeESP(plr)
     if Drawings[plr] then
         for _, obj in pairs(Drawings[plr]) do
@@ -119,7 +146,7 @@ local function removeESP(plr)
     end
 end
 
--- Tambah ESP
+-- Add ESP
 local function addESP(plr)
     if plr == game.Players.LocalPlayer then return end
 
@@ -145,7 +172,7 @@ local function addESP(plr)
     Drawings[plr] = {box, healthBar, nameText}
 
     game:GetService("RunService").RenderStepped:Connect(function()
-        if ESPEnabled and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") then
+        if plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") then
             local hrp = plr.Character.HumanoidRootPart
             local humanoid = plr.Character:FindFirstChildOfClass("Humanoid")
             local head = plr.Character:FindFirstChild("Head")
@@ -157,35 +184,48 @@ local function addESP(plr)
                     local width, height = 30 * scale, 50 * scale
 
                     -- Box ESP
-                    box.Size = Vector2.new(width, height)
-                    box.Position = Vector2.new(pos.X - width / 2, pos.Y - height / 2)
-                    box.Color = BoxColor
-                    box.Visible = true
-
-                    -- Healthbar vertikal kiri box
-                    local hpPercent = humanoid.Health / humanoid.MaxHealth
-                    local hbHeight = height * hpPercent
-                    local hbWidth = 4
-                    local hbX = pos.X - width / 2 - hbWidth - 2
-                    local hbY = pos.Y + height / 2 - hbHeight
-
-                    healthBar.Size = Vector2.new(hbWidth, hbHeight)
-                    healthBar.Position = Vector2.new(hbX, hbY)
-
-                    if hpPercent > 0.5 then
-                        healthBar.Color = Color3.fromRGB(0, 255, 0)
-                    elseif hpPercent > 0.2 then
-                        healthBar.Color = Color3.fromRGB(255, 255, 0)
+                    if BoxESPEnabled then
+                        box.Size = Vector2.new(width, height)
+                        box.Position = Vector2.new(pos.X - width / 2, pos.Y - height / 2)
+                        box.Color = BoxColor
+                        box.Visible = true
                     else
-                        healthBar.Color = Color3.fromRGB(255, 0, 0)
+                        box.Visible = false
                     end
-                    healthBar.Visible = true
 
-                    -- Nama di atas kepala
-                    local headPos, vis2 = workspace.CurrentCamera:WorldToViewportPoint(head.Position + Vector3.new(0, 1, 0))
-                    if vis2 then
-                        nameText.Position = Vector2.new(headPos.X, headPos.Y - 15)
-                        nameText.Visible = true
+                    -- Healthbar (kiri box)
+                    if HealthESPEnabled then
+                        local hpPercent = humanoid.Health / humanoid.MaxHealth
+                        local hbHeight = height * hpPercent
+                        local hbWidth = 4
+                        local hbX = pos.X - width / 2 - hbWidth - 2
+                        local hbY = pos.Y + height / 2 - hbHeight
+
+                        healthBar.Size = Vector2.new(hbWidth, hbHeight)
+                        healthBar.Position = Vector2.new(hbX, hbY)
+
+                        if hpPercent > 0.5 then
+                            healthBar.Color = Color3.fromRGB(0, 255, 0)
+                        elseif hpPercent > 0.2 then
+                            healthBar.Color = Color3.fromRGB(255, 255, 0)
+                        else
+                            healthBar.Color = Color3.fromRGB(255, 0, 0)
+                        end
+
+                        healthBar.Visible = true
+                    else
+                        healthBar.Visible = false
+                    end
+
+                    -- Name di atas kepala
+                    if NameESPEnabled then
+                        local headPos, vis2 = workspace.CurrentCamera:WorldToViewportPoint(head.Position + Vector3.new(0, 1, 0))
+                        if vis2 then
+                            nameText.Position = Vector2.new(headPos.X, headPos.Y - 15)
+                            nameText.Visible = true
+                        else
+                            nameText.Visible = false
+                        end
                     else
                         nameText.Visible = false
                     end
@@ -194,10 +234,6 @@ local function addESP(plr)
                     healthBar.Visible = false
                     nameText.Visible = false
                 end
-            else
-                box.Visible = false
-                healthBar.Visible = false
-                nameText.Visible = false
             end
         else
             box.Visible = false
@@ -207,33 +243,22 @@ local function addESP(plr)
     end)
 end
 
--- Toggle ESP
+-- Auto Apply ESP
+for _, plr in pairs(game.Players:GetPlayers()) do
+    addESP(plr)
+end
+game.Players.PlayerAdded:Connect(addESP)
+game.Players.PlayerRemoving:Connect(removeESP)
+
+-- UI Toggles
 VisualTab:CreateToggle({
-    Name = "ESP (Box + Name + Health)",
+    Name = "ESP Box",
     CurrentValue = false,
     Callback = function(Value)
-        ESPEnabled = Value
-        if ESPEnabled then
-            for _, plr in pairs(game.Players:GetPlayers()) do
-                if not Drawings[plr] then
-                    addESP(plr)
-                end
-            end
-            game.Players.PlayerAdded:Connect(function(plr)
-                addESP(plr)
-            end)
-            game.Players.PlayerRemoving:Connect(function(plr)
-                removeESP(plr)
-            end)
-        else
-            for _, plr in pairs(game.Players:GetPlayers()) do
-                removeESP(plr)
-            end
-        end
+        BoxESPEnabled = Value
     end,
 })
 
--- Color Picker ESP Box
 VisualTab:CreateColorPicker({
     Name = "Box Color",
     Color = Color3.fromRGB(0,255,0),
@@ -242,13 +267,18 @@ VisualTab:CreateColorPicker({
     end
 })
 
--- Max Zoom
-VisualTab:CreateSlider({
-   Name = "Max Zoom",
-   Range = {0, 1000},
-   Increment = 1,
-   CurrentValue = 128,
-   Callback = function(Value)
-      game.Players.LocalPlayer.CameraMaxZoomDistance = Value
-   end,
+VisualTab:CreateToggle({
+    Name = "ESP Healthbar",
+    CurrentValue = false,
+    Callback = function(Value)
+        HealthESPEnabled = Value
+    end,
+})
+
+VisualTab:CreateToggle({
+    Name = "ESP Name",
+    CurrentValue = false,
+    Callback = function(Value)
+        NameESPEnabled = Value
+    end,
 })
